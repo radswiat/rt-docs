172ff002fde5022e11cb00350656cb878914fe5d
radswiat <rad.swiat@gmail.com>
2017-02-17 20:42:57 +0000

    js parser

--- example_code/demo.js
+++ example_code/demo.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 /**
  * Test
  * @desc
@@ -5,4 +7,21 @@
  */
 class Test {
 
+  /**
+   * propTypes
+   * @property {children} ReactElement
+   */
+  static propTypes = {
+    children: React.PropTypes.element
+  };
+
+  /**
+   * Render react
+   * @protected
+   */
+  render() {
+    var s = 'dsaads';
+  }
+
 }
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import esprima from 'esprima';
+import { transform } from 'babel-core';
+import chalk from 'chalk';
+
 import { loadFile } from './utils';
 
 class RtDocs {
@@ -11,16 +14,82 @@ class RtDocs {
 
   }
 
-  async gen() {
+  async run() {
+    // await this.genBabylon();
+    await this.genEsprima();
+  }
+
+  async genBabylon() {
+    // console.log(chalk.green('babylon'));
+    // let file = await loadFile('example_code/demo.js');
+    // let parsedFile = await babylon.parse(file, {
+    //   sourceType: "module",
+    //   plugins: [
+    //     "jsx",
+    //     "flow"
+    //   ]
+    // });
+    // console.log(parsedFile);
+  }
+
+  async genEsprima() {
+    console.log(chalk.green('esprima'));
     let file = await loadFile('example_code/demo.js');
-    let parsedFile = esprima.parse(file, {
-      jsx: true,
-      comments: true
+    let parsed = transform(file, {
+      babelrc: false,
+      // compact: false,
+      // minified: false,
+      // moduleIds: true,
+      // retainLines: true,
+      // sourceMaps: true,
+      presets: [
+        "es2015",
+        "react",
+        "stage-0"
+      ],
+      plugins: [
+        "transform-runtime"
+      ]
     });
-    console.log(parsedFile);
+    // console.log(parsed.ast);
+    console.log(parsed.ast);
+
+    let tokens = parsed.ast.tokens;
+    tokens.forEach((token, i) => {
+
+
+      // find a comment block
+      if(token.type === 'CommentBlock') {
+
+        console.log(chalk.red('=============================================='));
+
+        let secToken = tokens[i+1];
+        let thToken = tokens[i+2];
+
+        // handle class comment block
+        if(secToken.type.label === 'class') {
+          console.log(chalk.yellow('class'));
+          console.log(`func name: ${thToken.value}`)
+        }else
+
+        // handle static comment block
+        if(secToken.type.label === 'name' && secToken.value === 'static') {
+          console.log(chalk.yellow('static'));
+          console.log(`func name: ${thToken.value}`)
+        }else
+
+        if(secToken.type.label === 'name') {
+          console.log(chalk.yellow('function'));
+          console.log(`func name: ${secToken.value}`)
+        }
+
+      }
+      // console.log('------');
+      // console.log(token);
+    })
   }
 
 }
 
 let rtDocs = RtDocs.create();
-rtDocs.gen();
+rtDocs.run();
